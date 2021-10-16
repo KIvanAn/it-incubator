233aaefa1c18adee9b1b18d4ca18801cf36ea034
ivan <kozel.vania2013@yandex.ru>
2021-10-16 19:04:45 +0300

    Components lesson

--- samurai/01-lesson/src/App.js
+++ samurai/01-lesson/src/App.js
@@ -2,15 +2,34 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <ul>
-        <li>Node 1</li>
-        <li>Node 2</li>
-        <li>Node 3</li>
-        <li>Node 4</li>
-      </ul>
+    <div>
+      <Header />
+      <List />
     </div>
   );
+}
+
+const Header = () => {
+  return (
+    <div>
+      <a href='#'>Home</a>
+      <a href='#'>About</a>
+      <a href='#'>Contact</a>
+    </div>
+  )
+}
+
+const List = () => {
+  return (
+    <div>
+        <ul>
+          <li>Node 1</li>
+          <li>Node 2</li>
+          <li>Node 3</li>
+          <li>Node 4</li>
+        </ul>
+      </div>
+  )
 }
 
 export default App;
