d594837c91baae41c2a625da0733f3b4b6ebf879
ivan <kozel.vania2013@yandex.ru>
2021-10-16 16:15:04 +0300

    lesson JSX

--- samurai/01-lesson/src/App.js
+++ samurai/01-lesson/src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ul>
+        <li>Node 1</li>
+        <li>Node 2</li>
+        <li>Node 3</li>
+        <li>Node 4</li>
+      </ul>
     </div>
   );
 }
