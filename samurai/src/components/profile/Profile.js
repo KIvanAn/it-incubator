78268b941e799f85a0751184f992fd8340b0d5f2
ivan <kozel.vania2013@yandex.ru>
2021-10-18 19:02:45 +0300

    Dialogs page

--- samurai/src/components/profile/Profile.js
+++ samurai/src/components/profile/Profile.js
@@ -3,7 +3,7 @@ import classes from './Profile.module.css'
 
 const Profile = () => {
 	return (
-		<div className={classes.content}>
+		<div>
 			<div>
 				<img src="https://images.unsplash.com/photo-1634325232058-e7b8f80cc20b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80" />
 			</div>
