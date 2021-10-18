a01600e28f51378756b0e750adb19cf185b05d54
ivan <kozel.vania2013@yandex.ru>
2021-10-18 19:15:20 +0300

    Freecodecamp day 3

---
+++ freecodecamp/day3/02.js
@@ -0,0 +1,46 @@
+var ourDecimal = 5.7;
+
+// Only change code below this line
+var myDecimal = 5.7
+
+var product = 2.0 * 2.5;
+var quotient = 4.4 / 2.0; // Change this line
+var remainder = 11 % 3;
+var a = 3;
+var b = 17;
+var c = 12;
+
+// Only change code below this line
+a += 12;
+b += 9;
+c += 7;
+
+var a = 11;
+var b = 9;
+var c = 3;
+
+// Only change code below this line
+a -= 6;
+b -= 15;
+c -= 1;
+
+var a = 5;
+var b = 12;
+var c = 4.6;
+
+// Only change code below this line
+a *= 5;
+b *= 3;
+c *= 10;
+
+var a = 48;
+var b = 108;
+var c = 33;
+
+// Only change code below this line
+a /= 12;
+b /= 4;
+c /= 11;
+
+var myFirstName = "Ivan";
+var myLastName = "Ivanov";
