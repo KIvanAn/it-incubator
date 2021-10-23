49f21f755a7d7d084b41a1c04992408b7d3df94c
ivan <kozel.vania2013@yandex.ru>
2021-10-23 20:23:32 +0300

    Codewars day 7

--- codewars/day7/01.js
+++ codewars/day7/01.js
@@ -14,3 +14,10 @@ var number = function(busStops){
     return allPass.reduce(reducer)
 }
 // console.log(number([[10,0],[3,5],[5,8]]))
+"use strict";
+function flattenAndSort(array) {
+    // Good luck, brave code warrior!
+    return array.flat().sort((a, b) => a - b);
+}
+
+console.log(flattenAndSort([[3, 2, 1], [7, 9, 8], [6, 4, 5]]))
