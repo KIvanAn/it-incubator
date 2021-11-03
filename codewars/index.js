91309d99033ede807274cd691259dde74a090618
ivan <kozel.vania2013@yandex.ru>
2021-11-03 16:11:27 +0300

    Users API

--- codewars/index.js
+++ codewars/index.js
@@ -1,12 +1,29 @@
-function solution(number) {
-    let res = 0
-    if (number > 0) {
-        res = Array(number)
-            .fill().map((el, i) => i)
-            .filter((el) => (el % 3 === 0) || (el % 5 === 0))
-            .reduce((a, b) => a + b)
+function findNumber(array) {
+    let res
+    for (let i = 0; i < array.length + 1; i++) {
+        let num = binarySearch(array.sort((a, b) => a - b), i + 1)
+        if (num === -1) {
+            res = i + 1
+            return res
+        }
     }
     return res
 }
 
-console.log(solution(0))
+function binarySearch(arr, val, start = 0, end = arr.length - 1) {
+    const mid = Math.floor((start + end) / 2);
+
+    if (val === arr[mid]) {
+        return mid;
+    }
+
+    if (start >= end) {
+        return -1;
+    }
+
+    return val < arr[mid]
+        ? binarySearch(arr, val, start, mid - 1)
+        : binarySearch(arr, val, mid + 1, end);
+}
+
+console.log(findNumber([1, 2, 3]))
