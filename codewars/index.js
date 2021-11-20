44c4c7a5edd40bca6ff6f6c7ed4968561c514cba
ivan <kozel.vania2013@yandex.ru>
2021-11-20 16:25:54 +0300

    Redux-form start

--- codewars/index.js
+++ codewars/index.js
@@ -1,13 +1,60 @@
-Array.prototype.sameStructureAs = function (other) {
-    return isArray(other) ? (req(this) === req(other)) : false
+function extractIds(data, res = []){
+    for (const [key, value] of Object.entries(data)) {
+        if (key === 'id') {
+            res.push(value)
+        } else {
+            return value.map(el => extractIds(el, res))
+        }
+    }
+    return res
 }
 
-function req (arr, dip = '-') {
-    return JSON.stringify([].concat(...arr.map((el, i) => isArray(el) ? req(el, dip.repeat(i + 1)) : dip)))
+let data = {
+    id: 1,
+    items: [
+        {id: 2},
+        {id: 3, items: [
+                {id: 4},
+                {id: 5}
+            ]}
+    ]
 }
 
-console.log([ 1, [ 1, 1 ] ].sameStructureAs( [ 2, [ 2, 2 ] ] ))
-
-function isArray(arr) {
-    return Array.isArray(arr)
+const data1 = {
+    id : 1,
+    items : [
+        {id : 2},
+        {id : 3}
+    ]
+};
+const data2 = {
+    id : 1,
+    items : [
+        {id : 2},
+        {id : 3, items : [
+                {id : 4},
+                {id : 5}
+            ]}
+    ]
 }
+const data3 = {
+    id : 1,
+    items : [{
+        id : 2,
+        items : [{
+            id : 3,
+            items : [
+                {id : 4},
+                {id : 5}
+            ]
+        },{
+            id : 6,
+            items : [{id : 7}]
+        }]
+    }]
+}
+
+console.log(extractIds(data))
+console.log(extractIds(data1))
+console.log(extractIds(data2))
+console.log(extractIds(data3))
--- samurai/src/components/login/Login.js
+++ samurai/src/components/login/Login.js
@@ -1,7 +1,37 @@
-const Login = () => {
+import {Field, reduxForm} from 'redux-form'
+
+const LoginForm = (props) => {
+    return (
+        <form onSubmit={props.handleSubmit}>
+            <div>
+                <Field component={'input'} name={'login'} type="text" placeholder={'Login'}/>
+            </div>
+            <div>
+                <Field component={'input'} name={'password'} type="text" placeholder={'Password'}/>
+            </div>
+            <div>
+                <Field component={'input'} name={'rememberMe'} type="checkbox"/>
+                <span>Remember me</span>
+            </div>
+            <div>
+                <button>Login</button>
+            </div>
+        </form>
+    )
+}
+
+const LoginReduxForm = reduxForm({
+    form: 'login',
+})(LoginForm)
+
+const Login = (props) => {
+    const onSubmit = (formData) => {
+        console.log(formData)
+    }
     return (
         <div>
-            <h1>Login page</h1>
+            <h1>Login</h1>
+            <LoginReduxForm onSubmit={onSubmit} />
         </div>
     )
 }
